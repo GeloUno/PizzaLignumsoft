87cabfbf7d273310e0ca1161c681ddd7a30e4d29
Grzegorz Kędziora <geloke59@gmail.com>
2023-08-12 19:27:54 +0200

    remove comment no code

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    console.log('pizza server :>> ');
     return { message: 'it is work pizza server!' };
   }
 }
