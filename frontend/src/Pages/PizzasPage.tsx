ac30a281aec64e93771d44a0aabf409c7c359aea
Grzegorz Kędziora <geloke59@gmail.com>
2023-08-13 16:31:21 +0200

    change name

--- frontend/src/Pages/PizzasPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function PizzasPage() {
-  return <div>pizzaPages</div>;
-}
-
-export default PizzasPage;
---
+++ frontend/src/pages/Pizza.page.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from 'react-query';
+import { getAllPizza } from '../controllers/pizza';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+import ListPizza from '../components/ListPizza';
+
+function PizzaPage() {
+  const { isLoading, isError, data } = useQuery(
+    'getAllPizza',
+    async () => await getAllPizza()
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  } else if (isError || data === undefined || data.data === undefined) {
+    return <Error />;
+  } else {
+    return <ListPizza data={data.data} />;
+  }
+}
+
+export default PizzaPage;
