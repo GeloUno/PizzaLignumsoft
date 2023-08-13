c26b96faa11043fc71a3f4804f31778a411c8932
Grzegorz Kędziora <geloke59@gmail.com>
2023-08-13 16:30:11 +0200

    fix proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,10 +2,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
+    ['/api', '/pizza/detail/api'],
     createProxyMiddleware({
       target: 'http://localhost:3001',
       changeOrigin: true,
+      pathRewrite: { 'pizza/detail': '' },
     })
   );
 };
