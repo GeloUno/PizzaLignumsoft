43e177bc8658f7442a79e43a0f302d94a5c7f0ef
Grzegorz Kędziora <geloke59@gmail.com>
2023-08-14 12:38:04 +0200

    use list product page

--- frontend/src/pages/Action.page.tsx
+++ frontend/src/pages/Action.page.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { useParams } from 'react-router';
-import Content from '../components/Content';
+import { useQuery } from 'react-query';
+import Loading from '../components/Loading';
 import Error from '../components/Error';
-import ActionDetail from '../components/ActionDetail';
+import NavBar from '../components/NavBar';
+import { getAllAction } from '../controllers/action';
+import ListProduct, { ProductEnum } from '../components/ListProduct';
 
 function ActionPage() {
-  const { id } = useParams();
-  if (id) {
-    return (
-      <Content margin={4} padding={4}>
-        <ActionDetail actionId={id} />
-      </Content>
-    );
-  } else {
+  const { isError, isLoading, data } = useQuery(
+    ['getAllAction'],
+    async () => await getAllAction()
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  } else if (isError || data === undefined || data.data === undefined) {
     return <Error />;
+  } else {
+    return (
+      <>
+        <NavBar />
+        <ListProduct data={data.data} type={ProductEnum.ACTION} />;
+      </>
+    );
   }
 }
 
