4ebc22d31626a356eba94e8926cc3586719c1490
Grzegorz Kędziora <geloke59@gmail.com>
2023-08-14 11:10:50 +0200

    changes in create proxy middlerware

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,11 +2,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    ['/api', '/pizza/detail/api'],
+    ['/api', '/pizza/detail/api', '/element/api', '/action/api'],
     createProxyMiddleware({
       target: 'http://localhost:3001',
       changeOrigin: true,
-      pathRewrite: { 'pizza/detail': '' },
+      pathRewrite: {
+        '^/pizza/detail': '',
+        '^/element': '',
+        '^/action': '',
+      },
     })
   );
 };
