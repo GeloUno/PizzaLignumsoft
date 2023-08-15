5c866dd231cc2ffbf9ed5910ccccbc6f27d288b1
Grzegorz Kędziora <geloke59@gmail.com>
2023-08-15 11:14:35 +0200

    add static server

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PizzaModule } from './pizza/pizza.module';
 import { ActionModule } from './action/action.module';
 import { ElementModule } from './element/element.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'frontend', 'build'),
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(
       `mongodb+srv://costamgdzieskiedys:${process.env.MONGODBPASSWORD}@cluster0.nhw9d3x.mongodb.net/`,
